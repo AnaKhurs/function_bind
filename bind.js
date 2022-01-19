cae2331adef01958021e4b28bcc23908ad19deaa
AnaKhurs <ana.khurs@gmail.com>
2022-01-19 21:12:45 +0300

    Update bind.js

--- bind.js
+++ bind.js
@@ -17,3 +17,32 @@ function bind (context, fn) {
 
 bind(person1, logPerson)()
 bind(person1, logPerson)()
+
+
+//proto
+let obj = {name: 'Evgen'};
+let obj2 = {
+    sayName() {
+        console.log(this.name);
+    }
+};
+obj2.sayName.bind(obj)();
+
+Function.prototype._bind = function(ctx, ...args) {
+       const _this = this;
+    return function(...args2) {
+        return _this.apply(ctx, [...args, ...args2]);
+    }
+}
+obj2.sayName._bind(obj)();
+
+Function.prototype.__bind = function (ctx, ...args) {
+    ctx.___someHidenMethod___ = this;
+    return function (...args2) {
+        return ctx.___someHidenMethod___(...args, ...args2);
+    }
+}
+
+//obj.sayName = obj2.sayName
+
+obj2.sayName.__bind(obj)();
